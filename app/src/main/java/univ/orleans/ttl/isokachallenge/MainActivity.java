8dc19fb2eb74fee9ada3170ba469c2a4782e28cf
Ribardiere Tom <tom.ribardiere@etu.univ-orleans.fr>
2020-10-28 17:03:16 +0100

    Test sur une méthode pour le Caroussel mais problème de resize des images car pas automatique, à revoir

--- app/src/main/java/univ/orleans/ttl/isokachallenge/MainActivity.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/MainActivity.java
@@ -3,11 +3,44 @@ package univ.orleans.ttl.isokachallenge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.imaginativeworld.whynotimagecarousel.CarouselItem;
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageCarousel carousel;
+
+    private  List<CarouselItem> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.carousel = findViewById(R.id.carousel);
+
+        this.list= new ArrayList<>();
+
+        list.add(
+                new CarouselItem(
+                         "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080",
+                         "Photo by Aaron Wu on Unsplash"
+                )
+        );
+        list.add(
+                new CarouselItem(
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080",
+                        "Photo by Johannes Plenio on Unsplash"
+                )
+        );
+        list.add(new CarouselItem("https://images-na.ssl-images-amazon.com/images/I/71wvedvViFL._AC_SY679_.jpg","Zero Two"));
+
+        carousel.addData(list);
+
+
+
     }
 }
