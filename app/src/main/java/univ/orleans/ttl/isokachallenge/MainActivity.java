52de657f88384d950bebeb47dc70cd60b6db1d8e
Ladislas WALCAK <walcak.ladislas@gmail.com>
2020-11-11 03:20:41 +0100

    [MOD] Fix Kotlin extensions used in java classes

--- app/src/main/java/univ/orleans/ttl/isokachallenge/MainActivity.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/MainActivity.java
@@ -3,11 +3,19 @@ package univ.orleans.ttl.isokachallenge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.time.LocalDateTime;
+
+import univ.orleans.ttl.isokachallenge.orm.*;
+import univ.orleans.ttl.isokachallenge.orm.entity.User;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DB db = new DB(this);
+        db.save(new User("Ladislus", "Admin", LocalDateTime.now()));
     }
 }
--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/entity/User.kt
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/entity/User.kt
@@ -1,11 +1,6 @@
 package univ.orleans.ttl.isokachallenge.orm.entity
 
-import android.content.ContentValues
-import android.util.Log
 import org.springframework.security.crypto.bcrypt.BCrypt
-import univ.orleans.ttl.isokachallenge.orm.*
-import java.lang.StringBuilder
-import java.sql.SQLException
 import java.time.LocalDateTime
 
 class User(internal var username: String, password: String, salt: String?, date: LocalDateTime) {
@@ -17,98 +12,4 @@ class User(internal var username: String, password: String, salt: String?, date:
     internal val date : String get() = this._date.toString()
 
     constructor(username: String, password: String, date: LocalDateTime) : this(username, password, null, date)
-}
-
-fun DB.save(user: User) {
-    try {
-        val values = ContentValues()
-        values.put(USER_NAME, user.username)
-        values.put(USER_PASSWORD, user.password)
-        values.put(USER_SALT, user.salt)
-        values.put(USER_DATE, user.date)
-
-        writableDatabase.insertOrThrow(USER_TABLE, null, values)
-        Log.d(DB_LOG, "SAVED USER ${user.username}")
-    } catch (ex: SQLException) {
-        Log.e(DB_LOG, "SAVE USER ${user.username} FAILED !")
-        ex.message?.let { Log.e(DB_LOG, it) }
-    }
-}
-
-fun DB.update(user: User) {
-    val values = ContentValues()
-    values.put(USER_NAME, user.username)
-    values.put(USER_PASSWORD, user.password)
-    values.put(USER_SALT, user.salt)
-    values.put(USER_DATE, user.date)
-
-    writableDatabase.update(USER_TABLE, values, "$USER_NAME = ${user.username}", null)
-}
-
-fun DB.delete(user: User) {
-    writableDatabase.delete(USER_TABLE, "$USER_NAME = ${user.username}", null)
-}
-
-fun DB.getAllUsers() : Collection<User> {
-    val list: MutableCollection<User> = ArrayList()
-
-    writableDatabase.query(
-                USER_TABLE,
-                arrayOf(USER_NAME, USER_PASSWORD, USER_SALT, USER_DATE),
-                null,
-                null,
-                null,
-                null,
-                "$USER_NAME ASC",
-            ).use {
-                while (it.moveToNext()) {
-                    list.add(User(it.getString(0), it.getString(1), it.getString(2), LocalDateTime.parse(it.getString(3))))
-                }
-            }
-
-    return list
-}
-
-fun DB.getUsers(wheres: Map<String, String>): Collection<User> {
-    return if (wheres.isEmpty()) getAllUsers()
-    else {
-        val list: MutableCollection<User> = ArrayList()
-
-        val sb = StringBuilder("WHERE 1")
-        for (s in wheres.keys) sb.append(" AND $s = ${wheres[s]}")
-
-        writableDatabase.query(
-                USER_TABLE,
-                arrayOf(USER_NAME, USER_PASSWORD, USER_SALT, USER_DATE),
-                sb.toString(),
-                null,
-                null,
-                null,
-                "$USER_NAME ASC",
-            ).use {
-                while (it.moveToNext()) {
-                    list.add(User(it.getString(0), it.getString(1), it.getString(2), LocalDateTime.parse(it.getString(3))))
-                }
-
-                list
-            }
-    }
-}
-
-fun DB.getUser(username: String): User? {
-    writableDatabase.query(
-            USER_TABLE,
-            arrayOf(USER_NAME, USER_PASSWORD, USER_SALT, USER_DATE),
-            "$USER_NAME = $username",
-            null,
-            null,
-            null,
-            null
-        ).use {
-            return if (it.moveToFirst()) {
-                User(it.getString(0), it.getString(1), it.getString(2), LocalDateTime.parse(it.getString(3)))
-            } else {
-                null
-            }
-    }
 }
