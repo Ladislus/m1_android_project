acc50c630b16fba76018738641d1c8c8712829da
Ladislas WALCAK <walcak.ladislas@gmail.com>
2020-11-11 21:07:01 +0100

    [MOD] Converted User.kt to User.java

---
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/entity/User.java
@@ -0,0 +1,27 @@
+package univ.orleans.ttl.isokachallenge.orm.entity;
+
+import java.time.LocalDateTime;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+public class User {
+
+    private String _userame;
+    private LocalDateTime _date;
+
+    public User(String username, LocalDateTime date) {
+        this._userame = username;
+        this._date = date;
+    }
+
+    public String getUsername() {
+        return this._userame;
+    }
+
+    public String getDate() {
+        return this._date.toString();
+    }
+
+    public static String hash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+    }
+}
