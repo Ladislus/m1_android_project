a7cfea117ce524337388dad92cdcb2c38adf6d86
Ladislus <walcak.ladislas@gmail.com>
2020-11-17 20:44:30 +0100

    [ADD] Added database column indexes

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/Tables.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/Tables.java
@@ -17,7 +17,9 @@ public class Tables {
 
     public static final String USER_TABLE = "USER";
     public static final String USER_NAME = "u_username";
+    public static final int USER_NAME_INDEX = 0;
     public static final String USER_DATE = "u_date";
+    public static final int USER_DATE_INDEX = 1;
 
     public static final String USER_CREATE = "CREATE TABLE " + USER_TABLE + "(" + USER_NAME + " VARCHAR(255) PRIMARY KEY," + USER_DATE + " DATE NOT NULL);";
 
@@ -27,8 +29,11 @@ public class Tables {
 
     public static final String DRAWING_TABLE = "DRAWING";
     public static final String DRAWING_ID = "d_id";
+    public static final int DRAWING_ID_INDEX = 0;
     public static final String DRAWING_LINK = "d_link";
+    public static final int DRAWING_LINK_INDEX = 1;
     public static final String DRAWING_DATE = "d_date";
+    public static final int DRAWING_DATE_INDEX = 2;
 
     public static final String DRAWING_CREATE = "CREATE TABLE " + DRAWING_TABLE + "("
             + DRAWING_ID + " INTEGER PRIMARY KEY,"
@@ -41,11 +46,17 @@ public class Tables {
 
     public static final String CHALLENGE_TABLE = "CHALLENGE";
     public static final String CHALLENGE_ID = "c_id";
+    public static final int CHALLENGE_ID_INDEX = 0;
     public static final String CHALLENGE_NAME = "c_name";
+    public static final int CHALLENGE_NAME_INDEX = 1;
     public static final String CHALLENGE_TYPE = "c_type";
+    public static final int CHALLENGE_TYPE_INDEX = 2;
     public static final String CHALLENGE_THEME = "c_theme";
+    public static final int CHALLENGE_THEME_INDEX = 3;
     public static final String CHALLENGE_DURATION = "c_duration";
+    public static final int CHALLENGE_DURATION_INDEX = 4;
     public static final String CHALLENGE_TIMER = "c_timer";
+    public static final int CHALLENGE_TIMER_INDEX = 5;
 
     public static final String CHALLENGE_CREATE = "CREATE TABLE " + CHALLENGE_TABLE + "("
             + CHALLENGE_ID + " INTEGER PRIMARY KEY,"
@@ -61,10 +72,15 @@ public class Tables {
 
     public static final String PARTICIPATION_TABLE = "PARTICIPATION";
     public static final String PARTICIPATION_CHALLENGE_ID = "p_c_id";
+    public static final int PARTICIPATION_CHALLENGE_ID_INDEX = 0;
     public static final String PARTICIPATION_USER_ID = "p_u_id";
+    public static final int PARTICIPATION_USER_ID_INDEX = 1;
     public static final String PARTICIPATION_DRAWING_ID = "p_d_id";
+    public static final int PARTICIPATION_DRAWING_ID_INDEX = 2;
     public static final String PARTICIPATION_IS_CREATOR = "p_is_creator";
+    public static final int PARTICIPATION_IS_CREATOR_INDEX = 3;
     public static final String PARTICIPATION_VOTES = "p_votes";
+    public static final int PARTICIPATION_VOTES_INDEX = 4;
 
     public static final String PARTICIPATION_CREATE = "CREATE TABLE " + PARTICIPATION_TABLE + "("
             + PARTICIPATION_USER_ID + " VARCHAR(255),"
