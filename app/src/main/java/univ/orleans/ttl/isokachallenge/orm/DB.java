20196e2045e17720ec05f647f72add40a523da43
Ladislus <walcak.ladislas@gmail.com>
2020-11-18 00:37:47 +0100

    [MOD] Sanitized imports

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/DB.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/DB.java
@@ -11,9 +11,7 @@ import android.util.Pair;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 import univ.orleans.ttl.isokachallenge.orm.entity.*;
