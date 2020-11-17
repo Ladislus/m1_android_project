a09ddd281cd588612f45b86d971169e842617a47
Ladislus <walcak.ladislas@gmail.com>
2020-11-18 00:32:49 +0100

    [ADD] Added operator for where

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/Tables.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/Tables.java
@@ -63,7 +63,7 @@ public class Tables {
             + CHALLENGE_NAME + " VARCHAR(255) NOT NULL,"
             + CHALLENGE_TYPE + " BOOLEAN NOT NULL,"
             + CHALLENGE_THEME + " VARCHAR(255) NOT NULL,"
-            + CHALLENGE_DURATION + " INTEGER NOT NULL,"
+            + CHALLENGE_DURATION + " DATE NOT NULL,"
             + CHALLENGE_TIMER + " INTEGER NOT NULL);";
 
     //  ##########################
@@ -71,12 +71,12 @@ public class Tables {
     //  ##########################
 
     public static final String PARTICIPATION_TABLE = "PARTICIPATION";
-    public static final String PARTICIPATION_CHALLENGE_ID = "p_c_id";
-    public static final int PARTICIPATION_CHALLENGE_ID_INDEX = 0;
     public static final String PARTICIPATION_USER_ID = "p_u_id";
-    public static final int PARTICIPATION_USER_ID_INDEX = 1;
+    public static final int PARTICIPATION_USER_ID_INDEX = 0;
     public static final String PARTICIPATION_DRAWING_ID = "p_d_id";
-    public static final int PARTICIPATION_DRAWING_ID_INDEX = 2;
+    public static final int PARTICIPATION_DRAWING_ID_INDEX = 1;
+    public static final String PARTICIPATION_CHALLENGE_ID = "p_c_id";
+    public static final int PARTICIPATION_CHALLENGE_ID_INDEX = 2;
     public static final String PARTICIPATION_IS_CREATOR = "p_is_creator";
     public static final int PARTICIPATION_IS_CREATOR_INDEX = 3;
     public static final String PARTICIPATION_VOTES = "p_votes";
@@ -101,4 +101,16 @@ public class Tables {
             + "DROP TABLE IF EXISTS " + USER_TABLE + ";"
             + "DROP TABLE IF EXISTS " + DRAWING_TABLE + ";"
             + "DROP TABLE IF EXISTS " + CHALLENGE_TABLE + ";";
+
+    //  ############
+    //  #   OTHER  #
+    //  ############
+
+    public static final String OPERATOR_EQ = " = ";
+    public static final String OPERATOR_DIFF = " <> ";
+    public static final String OPERATOR_INF = " < ";
+    public static final String OPERATOR_INF_EQ = " <= ";
+    public static final String OPERATOR_SUP = " > ";
+    public static final String OPERATOR_SUP_EQ = " >= ";
+    public static final String OPERATOR_LIKE = " LIKE ";
 }
