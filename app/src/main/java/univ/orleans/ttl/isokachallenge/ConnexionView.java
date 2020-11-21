9c52d8be5274622862831b7e9280172c5a0bb31c
Thomas Q <mamangvomi@gmail.com>
2020-11-21 13:38:26 +0100

    [ADD] Login view created

---
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/ConnexionView.java
@@ -0,0 +1,14 @@
+package univ.orleans.ttl.isokachallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnexionView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion_view);
+    }
+}
