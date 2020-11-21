fff0b5e051e99f875a3d22a77c383e533c025be4
Tom <tom.ribardiere@free.fr>
2020-11-21 18:35:02 +0100

    [ADD] Sort draw off a challenge, more recent first

--- app/src/main/java/univ/orleans/ttl/isokachallenge/MainActivity.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/MainActivity.java
@@ -1,8 +1,10 @@
 package univ.orleans.ttl.isokachallenge;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -10,10 +12,17 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
+import java.util.SortedMap;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,42 +67,42 @@ public class MainActivity extends AppCompatActivity {
         ImageDessin imageDessinTourEiffel = new ImageDessin();
         imageDessinTourEiffel.imageUrl = "https://www.infinityandroid.com/images/france_eiffel_tower.jpg";
         imageDessinTourEiffel.auteur = "France";
-        imageDessinTourEiffel.dateSoumission = "Eiffel Tower";
+        imageDessinTourEiffel.dateSoumission = "08/11/2020 à 18h12";
         imageDessinTourEiffel.startRating = 48;
         imageDessins.add(imageDessinTourEiffel);
 
         ImageDessin imageDessinMoutainView = new ImageDessin();
         imageDessinMoutainView.imageUrl = "https://www.infinityandroid.com/images/indonesia_mountain_view.jpg";
         imageDessinMoutainView.auteur = "Indonesia";
-        imageDessinMoutainView.dateSoumission = "Mountain View";
+        imageDessinMoutainView.dateSoumission = "08/11/2020 à 18h12";
         imageDessinMoutainView.startRating = 45;
         imageDessins.add(imageDessinMoutainView);
 
         ImageDessin imageDessinTajMahal = new ImageDessin();
         imageDessinTajMahal.imageUrl = "https://www.infinityandroid.com/images/india_taj_mahal.jpg";
         imageDessinTajMahal.auteur = "India";
-        imageDessinTajMahal.dateSoumission = "Taj Mahal";
+        imageDessinTajMahal.dateSoumission = "08/11/2020 à 18h12";
         imageDessinTajMahal.startRating = 43;
         imageDessins.add(imageDessinTajMahal);
 
         ImageDessin imageDessinLakeView = new ImageDessin();
         imageDessinLakeView.imageUrl = "https://www.infinityandroid.com/images/canada_lake_view.jpg";
         imageDessinLakeView.auteur = "Canada";
-        imageDessinLakeView.dateSoumission = "Lake View";
+        imageDessinLakeView.dateSoumission = "21/11/2020 à 1h12";
         imageDessinLakeView.startRating = 10 ;
         imageDessins.add(imageDessinLakeView);
 
         ImageDessin imageDessinTest = new ImageDessin();
         imageDessinTest.imageUrl = "https://images-na.ssl-images-amazon.com/images/I/71wvedvViFL._AC_SY679_.jpg";
         imageDessinTest.auteur = "Tom99";
-        imageDessinTest.dateSoumission = "8/11/2020 à 18h12";
+        imageDessinTest.dateSoumission = "21/11/2020 à 16h12";
         imageDessinTest.startRating = 42;
         imageDessins.add(imageDessinTest);
 
         ImageDessin imageDessinTestAnime = new ImageDessin();
         imageDessinTestAnime.imageUrl = "https://wallpapercave.com/wp/wp4443741.jpg";
         imageDessinTestAnime.auteur = "Paysage";
-        imageDessinTestAnime.dateSoumission = "Landscape";
+        imageDessinTestAnime.dateSoumission = "08/11/2020 à 18h12";
         imageDessinTestAnime.startRating = 45;
         imageDessins.add(imageDessinTestAnime);
 
@@ -104,6 +114,28 @@ public class MainActivity extends AppCompatActivity {
         imageDessinAnime.dateSoumission = "8/11/2020 à 18h10";
         imageDessinAnime.startRating = 450;
         test.add(imageDessinAnime);
+
+        imageDessins.sort((o1, o2) -> {
+            try {
+                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy 'à' hh'h'mm");
+                Date d1 = format.parse(o1.dateSoumission);
+                Date d2 = format.parse(o2.dateSoumission);
+                if(d1.after(d2) ) {
+                    Log.d("Sort","1");
+                    return -1;
+                } else if(d1.before(d2)) {
+                    Log.d("Sort","-1");
+                    return 1;
+                } else {
+                    Log.d("Sort","0");
+                    return 0;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            Log.d("Sort","10");
+            return 10;
+        });
 
 
         Challenge challenge1 = new Challenge("Test",imageDessins);
