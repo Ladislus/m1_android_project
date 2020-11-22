7fc620750ff47565685b80272f5b4eb70db85f14
Thomas Q <mamangvomi@gmail.com>
2020-11-22 13:25:07 +0100

    [ADD] input de connexion + le checkbox 'Se souvenir de moi' qui fonctionne

--- app/src/main/java/univ/orleans/ttl/isokachallenge/ConnexionView.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/ConnexionView.java
@@ -2,13 +2,64 @@ package univ.orleans.ttl.isokachallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class ConnexionView extends AppCompatActivity {
+
+    private EditText mdp, login;
+    private CheckBox checkLogin;
+    private boolean remember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion_view);
+        this.login = findViewById(R.id.inputLogin);
+        this.mdp = findViewById(R.id.inputMDP);
+        this.checkLogin = findViewById(R.id.checkLogin);
+        this.remember = false;
+    }
+
+    public void onConnexion(View view) {
+        Log.d("login", "login : "+this.login.getText()+" / mdp : "+this.mdp.getText());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        if(this.checkLogin.isChecked()){
+            editor.putString("login", String.valueOf(this.login.getText()));
+            editor.putString("mdp", String.valueOf(this.mdp.getText()));
+        }else{
+            editor.putString("login", "");
+            editor.putString("mdp", "");
+        }
+        editor.putBoolean("isChecked", this.checkLogin.isChecked());
+        editor.apply();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        this.login.setText(sharedPref.getString("login",""));
+        this.mdp.setText(sharedPref.getString("mdp",""));
+        this.remember = sharedPref.getBoolean("isChecked",false);
+        this.checkLogin.setChecked(this.remember);
+
+    }
+
+    public void onChecked(View view) {
+        this.remember = this.checkLogin.isChecked();
     }
 }
