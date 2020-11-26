21cacab4f33d517474ccf73c874869c8e8667de6
Ladislus <walcak.ladislas@gmail.com>
2020-11-26 21:49:21 +0100

    [DEL] Removed unused imports

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/DB.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/DB.java
@@ -8,11 +8,6 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import android.graphics.Bitmap;
-
-import java.io.ByteArrayOutputStream;
-
-import android.util.Base64;
 import android.util.Log;
 import android.util.Pair;
 
