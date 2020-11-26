0eb8fd8e86ed8a55d68c3ea9338226c1f1bc7df2
Ladislus <walcak.ladislas@gmail.com>
2020-11-26 21:45:35 +0100

    [ADD] Added upload to imgur

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/RequestWrapper.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/RequestWrapper.java
@@ -1,6 +1,15 @@
 package univ.orleans.ttl.isokachallenge.orm;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.util.Base64;
+import android.util.Log;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import java.io.ByteArrayOutputStream;
 
 public class RequestWrapper {
 
@@ -12,8 +21,18 @@ public class RequestWrapper {
 
     public RequestWrapper(SQLiteDatabase db) { this._db = db; }
 
-    public String imgurUpload(String b64Image) {
-        return "";
+    public void imgurUpload(Bitmap image, JSONObjectRequestListener callback) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        String b64Image = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+
+        Log.d(Tables.DB_LOG + "1", b64Image);
+        AndroidNetworking.post(imgurAPI)
+                .addHeaders("Authorization", "Client-ID " + clientId)
+                .addBodyParameter("image", b64Image)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(callback);
     }
 }
 
