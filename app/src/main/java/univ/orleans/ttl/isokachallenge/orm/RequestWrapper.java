a6fbf4e5283d9148296955eb1b66ca94ec8a6978
Ladislus <walcak.ladislas@gmail.com>
2020-11-26 22:49:08 +0100

    [ADD] Adde apiKey & untested login method

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/RequestWrapper.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/RequestWrapper.java
@@ -14,7 +14,9 @@ public class RequestWrapper {
 
     private static final String _imgurAPI = "https://api.imgur.com/3/image";
     private static final String _clientId = "e70fe5ed4d03b66";
+
     private static final String _serverAPI = "https://thlato.pythonanywhere.com/api/";
+    private static final String _apiKey = "h1ZTSY38h4hAjWn5yFeBJ1MVw4VXjienv6ksBXV0Ek7hh3qo2A";
 
     public void imgurUpload(Bitmap image, JSONObjectRequestListener callback) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -24,6 +26,16 @@ public class RequestWrapper {
         AndroidNetworking.post(_imgurAPI)
                 .addHeaders("Authorization", "Client-ID " + _clientId)
                 .addBodyParameter("image", b64Image)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(callback);
+    }
+
+    public void login(String username, String password, JSONObjectRequestListener callback) {
+        AndroidNetworking.post(_serverAPI + "login")
+                .addHeaders("apiKey", _apiKey)
+                .addBodyParameter("username", username)
+                .addBodyParameter("password", password)
                 .setPriority(Priority.HIGH)
                 .build()
                 .getAsJSONObject(callback);
