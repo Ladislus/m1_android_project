94ef2754c4d8cde96e7faf33eb99fd0d5ec69a14
Tom <tom.ribardiere@free.fr>
2020-11-27 11:19:31 +0100

    [ADD] Liaison BD distante avec fonction Ladislas

--- app/src/main/java/univ/orleans/ttl/isokachallenge/Profil.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/Profil.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import univ.orleans.ttl.isokachallenge.orm.RequestWrapper;
 import univ.orleans.ttl.isokachallenge.orm.entity.User;
 
 import android.Manifest;
@@ -17,14 +18,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.google.android.material.navigation.NavigationView;
+
+import org.json.JSONObject;
 
 public class Profil extends AppCompatActivity {
 
-    private TextView username, password, msgRetourValidation;
+    private TextView username, msgRetourValidation;
     private EditText edit_password;
-    private Button modifier, valider;
+    private Button modifier, valider, validerMDP;
     private SharedPreferences pref;
+    private String old_pass;
+
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
@@ -81,10 +88,10 @@ public class Profil extends AppCompatActivity {
         pref = getApplicationContext().getSharedPreferences("session", MODE_PRIVATE);
 
         username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
         edit_password = findViewById(R.id.inputNewPassword);
         modifier = findViewById(R.id.btn_modifier);
         valider = findViewById(R.id.btn_valider);
+        validerMDP = findViewById(R.id.btn_mdp);
         msgRetourValidation = findViewById(R.id.msgRetourValidation);
         username.setText(username.getText()+" "+pref.getString("username",null));
     }
@@ -98,35 +105,37 @@ public class Profil extends AppCompatActivity {
     }
 
     public void modifier_username(View view) {
-        modifier.setVisibility(View.GONE);
-        username.setVisibility(View.GONE);
+        username.setText("Enter your actual password :");
+        old_pass = edit_password.getText().toString();
 
-        password.setVisibility(View.VISIBLE);
-        edit_password.setVisibility(View.VISIBLE);
-        valider.setVisibility(View.VISIBLE);
+
     }
 
-    public void valider_username(View view) {
+    public void valider_new_mdp(View view) {
         //TODO
-//        Boolean bool =  MainActivity.db.updatePassword(pref.getString("username",null),edit_password.getText().toString());
-        Boolean bool = true;
 
-        username.setVisibility(View.VISIBLE);
+        username.setText(getResources().getString(R.string.pseudoTitre)+pref.getString("username",null));
         modifier.setVisibility(View.VISIBLE);
         msgRetourValidation.setVisibility(view.VISIBLE);
 
         edit_password.setVisibility(View.GONE);
-        password.setVisibility(View.GONE);
         valider.setVisibility(View.GONE);
 
-        if (bool){
-            msgRetourValidation.setText(R.string.text_validation_password_updated);
-            msgRetourValidation.setTextColor(ResourcesCompat.getColor(getResources(), R.color.green, null));
-        }else{
-            msgRetourValidation.setText(R.string.txt_validate_password_not_updated);
-            msgRetourValidation.setTextColor(ResourcesCompat.getColor(getResources(), R.color.red, null));
-        }
+        RequestWrapper requestWrapper = new RequestWrapper();
+        JSONObjectRequestListener callback = new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                msgRetourValidation.setText(R.string.text_validation_password_updated);
+                msgRetourValidation.setTextColor(ResourcesCompat.getColor(getResources(), R.color.green, null));
+            }
 
+            @Override
+            public void onError(ANError anError) {
+                msgRetourValidation.setText(R.string.txt_validate_password_not_updated);
+                msgRetourValidation.setTextColor(ResourcesCompat.getColor(getResources(), R.color.red, null));
+            }
+        };
+        requestWrapper.updatePassword(pref.getString("userame",null),old_pass,edit_password.getText().toString(),callback);
 
     }
 
@@ -140,5 +149,21 @@ public class Profil extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24);
+    }
+
+    public void verif_mdp(View view) {
+        username.setText("Enter your actual password :");
+
+        edit_password.setVisibility(View.VISIBLE);
+        modifier.setVisibility(View.GONE);
+        validerMDP.setVisibility(View.VISIBLE);
+    }
+
+    public void new_mpd(View view) {
+        old_pass = edit_password.getText().toString();
+        edit_password.setText("");
+        username.setText("Enter your new password :");
+        validerMDP.setVisibility(View.GONE);
+        valider.setVisibility(View.VISIBLE);
     }
 }
