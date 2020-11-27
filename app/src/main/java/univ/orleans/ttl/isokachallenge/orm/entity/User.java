d02519a23d757d3118d21943ad19accb0930327e
Ladislus <walcak.ladislas@gmail.com>
2020-11-27 23:01:02 +0100

    [ADD] Added some comments for User

--- app/src/main/java/univ/orleans/ttl/isokachallenge/orm/entity/User.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/orm/entity/User.java
@@ -1,5 +1,7 @@
 package univ.orleans.ttl.isokachallenge.orm.entity;
 
+import androidx.annotation.NonNull;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -7,19 +9,33 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
+/**
+ * Représentation d'un compte utilisateur
+ */
 public class User {
 
     private final String _username;
     private final LocalDateTime _date;
     private final String _salt;
 
-    public User(String username, LocalDateTime date, String salt) {
+    /**
+     * Constructeur par défaut
+     * @param username Le nom d'utilisateur
+     * @param date La date d'inscription
+     * @param salt Le sel utilisé pour hasher le mot de passe
+     */
+    public User(@NonNull String username, @NonNull LocalDateTime date, @NonNull String salt) {
         this._username = username;
         this._date = date;
         this._salt = salt;
     }
 
-
+    /**
+     * Constructeur pour créer un nouvel utilisateur
+     * Ce constructeur génère le salt de l'utilisateur
+     * @param username Le nom d'utilisateur
+     * @param date La date d'inscription
+     */
     public User(String username, LocalDateTime date) {
         this(username, date, BCrypt.gensalt(12));
     }
@@ -40,6 +56,11 @@ public class User {
         return this._date.toString();
     }
 
+    /**
+     * Getter de la date d'inscription de l'utilisateur, au format "jour/mois/année, heure'minute'seconde"
+     * Exemple: 29/10/2020, 19'20'00
+     * @return La date d'inscription de l'utilisateur
+     */
     public String getFormattedDate() {
         return this._date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy, HH'h'mm"));
     }
@@ -48,10 +69,21 @@ public class User {
         return this._salt;
     }
 
+    /**
+     * Fonction de hashage de mot de passe avec BCrypt
+     * @param password Le mot de passe en clair
+     * @param salt Le salt à utiliser
+     * @return Le mot de passe hashé avec BCrypt
+     */
     public static String hash(String password, String salt) {
         return BCrypt.hashpw(password, salt);
     }
 
+    /**
+     * Method de transformation de l'instance d
+     * @param password Le mot de pa
+     * @return L'utilisateur au format JSONObject
+     */
     public JSONObject toJson(String password) {
         try {
             return new JSONObject()
