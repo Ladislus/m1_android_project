baf942ec1815ba5692569a9db40c378c5d88479e
Ladislus <walcak.ladislas@gmail.com>
2020-11-28 23:30:45 +0100

    [MOD] Added request management for vote

--- app/src/main/java/univ/orleans/ttl/isokachallenge/ParticipationAdapteur.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/ParticipationAdapteur.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,13 +25,12 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import univ.orleans.ttl.isokachallenge.orm.DB;
 import univ.orleans.ttl.isokachallenge.orm.RequestWrapper;
-import univ.orleans.ttl.isokachallenge.orm.Tables;
 import univ.orleans.ttl.isokachallenge.orm.entity.Participation;
 
 public class ParticipationAdapteur extends RecyclerView.Adapter<ParticipationAdapteur.MyViewHolder> {
 
-    private Context mContext;
-    private List<Participation> mParticipation;
+    private final Context mContext;
+    private final List<Participation> mParticipation;
 
     /**
      * Constructeur des l'adapteur
@@ -43,6 +41,7 @@ public class ParticipationAdapteur extends RecyclerView.Adapter<ParticipationAda
         this.mContext = mContext;
         this.mParticipation = mParticipation;
     }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -58,49 +57,54 @@ public class ParticipationAdapteur extends RecyclerView.Adapter<ParticipationAda
         holder.pseudo.setText(mParticipation.get(position).getUser().getUsername());
         Picasso.get().load(mParticipation.get(position).getDrawing().getLink()).into(holder.dessin);
         holder.votes.setText(mParticipation.get(position).getVotes().toString());
+
         // recupération shared pref
         SharedPreferences sharedPref = mContext.getSharedPreferences("session", Context.MODE_PRIVATE);
-        if( !(sharedPref.getString("username","").equals(""))) { // si connecté
-            holder.participationItem.setOnClickListener(v -> { // mettre un listener
+        // Si connecté ...
+        if( !(sharedPref.getString("username","").equals(""))) {
+            // ... mettre un listener sur la card qui represent la participation à la position "position"
+            holder.participationItem.setOnClickListener(v -> {
 
-                //TODO Use request
-                try { // synchro BD local et distante
+                try {
+                    // Lancement requête de vote pour la particition mParticition[position]
                     new RequestWrapper().vote(mParticipation.get(position).toJson(), sharedPref.getString("username", ""), new JSONObjectRequestListener() {
+                        // Si la requête réussie ...
                         @Override
                         public void onResponse(JSONObject response) {
                             try {
-                                // gestion du vote
+                                // ... mise à jour de la participation dans la base de données locale
                                 DB.getInstance().update(Participation.fromJson(response));
-                                // remplacement de paraticipation avec l'ancien vote avec le nouveau
+                                // ... remplacement de paraticipation avec l'ancien vote avec le nouveau
+                                // (Participation est immuable)
                                 mParticipation.remove(position);
                                 mParticipation.add(position,Participation.fromJson(response));
-                                // maj affichage vote
+                                //  ... mise à jour de l'affichage vote
                                 holder.votes.setText(mParticipation.get(position).getVotes().toString());
                             } catch (JSONException e) {
-                                Log.d("bonjour", "onResponse: salut");
+                                // Si le parse de la participation actuel en JSON échoue,
+                                // on annule la requête
                                 e.printStackTrace();
                             }
                         }
 
+                        // Si la requête échoue
                         @Override
                         public void onError(ANError anError) {
-                            Log.d(RequestWrapper.REQUEST_LOG, "onError: "+anError.toString());
-                            Log.d(RequestWrapper.REQUEST_LOG, "onError: "+anError.getErrorDetail());
-                            Log.d(RequestWrapper.REQUEST_LOG, "onError: "+anError.getErrorBody());
-                            Log.d(RequestWrapper.REQUEST_LOG, "onError: "+anError.getErrorCode());
+                            // Si l'utilisateur a déjà voté
                             if (anError.getErrorCode() == 409) {
-                                Log.d(RequestWrapper.REQUEST_LOG, "Déjà voté");
-                                Toast.makeText(mContext,R.string.toastVoteDeja, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(mContext, R.string.toastVoteDeja, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(mContext, R.string.communication_error, Toast.LENGTH_LONG).show();
                             }
                         }
                     });
                 } catch (JSONException e) {
+                    // Si le parse de la participation échoue,
+                    // on annule la requête
                     e.printStackTrace();
                 }
-
-                //holder.votes.setText(mParticipation.get(position).getVotes().toString());
             });
-        }else{
+        } else {
             Toast.makeText(mContext, R.string.toatConnexionVote, Toast.LENGTH_LONG).show();
         }
     }
