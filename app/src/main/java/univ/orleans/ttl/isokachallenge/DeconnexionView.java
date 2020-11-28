a18fe585a7a77d24aa7be4740c5819d50001ef57
Ladislus <walcak.ladislas@gmail.com>
2020-11-29 00:59:45 +0100

    [MOD] Clean DeconnexionView

--- app/src/main/java/univ/orleans/ttl/isokachallenge/DeconnexionView.java
+++ app/src/main/java/univ/orleans/ttl/isokachallenge/DeconnexionView.java
@@ -5,11 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-import com.google.android.material.navigation.NavigationView;
+import android.os.Bundle;
 
 public class DeconnexionView extends AppCompatActivity {
 
@@ -17,22 +14,16 @@ public class DeconnexionView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deconnexion_view);
-        SharedPreferences sharedPref = this.getSharedPreferences("session", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // Lors de la déconnexion ...
+        SharedPreferences.Editor editor = this.getSharedPreferences("session", Context.MODE_PRIVATE).edit();
+        // ... Clear l'utilisateur actuellement connecté
         editor.putString("username", "");
         editor.apply();
+
+        // Puis retour à la MainActivity
         finish();
         Intent home = new Intent(this, MainActivity.class);
         startActivity(home);
     }
-
-//    public void onGoBack(View view) {
-//        /**
-//         * Fonction appelé lors du clique sur le bouton "Retour à l'acceuil" dans
-//         * l'activity DeconnexionView
-//         */
-//        finish();
-//        Intent home = new Intent(this, MainActivity.class);
-//        startActivity(home);
-//    }
 }
